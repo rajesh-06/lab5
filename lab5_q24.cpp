0849dce8e7f919ed66d77758d51569a14e364ffb
rajesh-06 <42206081+rajesh-06@users.noreply.github.com>
2018-08-31 19:19:38 +0530

    Add files via upload

---
+++ lab5_q24.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	//declare the variable
+	char ch = 'a';
+	//print all the alphabet
+	cout << "Alphabet from a - z are: "<<endl;
+	while (ch<= 'z')
+  	{
+	ch++;
+	cout << ch <<endl ;
+	}
+// terminating program
+return 0;
+
+}
---
+++ lab5_q25.cpp
@@ -0,0 +1,18 @@
+//library
+#include<iostream>
+using namespace std;
+// main function
+int main(){
+	//Declare the variable
+	int i =2	
+	//printing all the even numbers from 1 to 100 
+	cout << "All the even numbers from 1 to 100 is " << endl;
+	while ( i<=100)
+	{cout << i << endl;
+	i = i +2;
+	}
+	
+// terminating program
+return 0;
+
+}
